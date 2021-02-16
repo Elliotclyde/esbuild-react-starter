e639182db7100f1d8d29ef48971261e3106fbf24
Hugh Haworth <hughelliotclyde@gmail.com>
2021-02-16 17:42:25 +1300

    Removed servor dependency

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom'
+import './style.css';
 
 const App = ()=>{
-    return <h1>esbuild react starter</h1>
+    return <>
+    <h1>Welcome</h1>
+    <p>To React esbuild starter</p>
+    </>
 }
 
-ReactDOM.render(<App/>,document.getElementById('root')); 
+ReactDOM.render(<App/>,document.getElementById('root'));
